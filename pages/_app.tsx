713d5bac6fc9730ccab21c28d4b54a10372474df
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-12-28 04:16:41 +0300

    Initial setup and darkmode

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+
+import "@/styles/globals.css";
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+    <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+    </ThemeProvider>
+);
+
+export default MyApp;
